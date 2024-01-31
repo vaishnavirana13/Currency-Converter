f399255c8e3a12beb754f66cf861bc7222dd27bb
Vaishnavi  Rana <vaishnavirana5@gmail.com>
2024-01-31 08:59:49 +0530

    added more functionalities
    "more functionsalities"

--- Cuc.js
+++ Cuc.js
@@ -1,8 +1,11 @@
-const BASE_URL = "https:cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json"
+const BASE_URL =  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
 
 const  dropdowns =  document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select")
+const toCurr = document.querySelector(".to select")
+const msg = document.querySelector(".msg");
 // for (names in countryList){
 // 	console.log(names);
 // }
@@ -13,11 +16,11 @@ for (let select of dropdowns) {
 	  newOption.innerText = currCode ;
 	  newOption.value = currCode ;
 	  if (select.name === "from" && currCode === "USD") {
-		newOption.selected = "selected"
-	  } else if(select.name === "To" && currCode === "INR") {
-		newOption.selected = "selected"
+		newOption.selected = "selected";
+	  } else if(select.name === "to" && currCode === "INR") {
+		newOption.selected = "selected";
 	  }
-	  select.append(newOption)
+	  select.append(newOption);
 	}
 	select.addEventListener("change" , (evt) => {
 		updateFlag(evt.target);
@@ -38,13 +41,22 @@ const updateFlag = (element) => {
 
 
 // exchane button function
-btn.addEventListener("click",(evt)=>{
-     evt.preventDefault;
-	 let amount = document.querySelector("form input" );
+btn.addEventListener("click", async(evt)=>{
+     evt.preventDefault();
+	 let amount = document.querySelector(".amount input");
 	 let amtval = amount.value;
 	  if (amtval === "" || amtval < 1) {
 		amtval = 1 ;
 		amount.value = "1";
-		
+
 	  }
+
+	  const URL  =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
+	  let response = await fetch(URL);
+	  let data = await response.json();
+	  let rate = data[toCurr.value.toLowerCase()]
+	  console.log(rate);
+
+	  let finalAmount = amtval * rate ;
+	  msg.innerText = `${amtval} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
 })
